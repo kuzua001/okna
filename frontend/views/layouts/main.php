b417bc99ce66bfae37c759fd979b5976c67422fb
Ivan Kuznetsov <kuznecov.fd0@yandex.ru>
2017-07-13 00:21:52 +0300

    Пофиксил хедер, вот теперь все работает

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -11,24 +11,25 @@ use frontend\assets\AppAsset;
 use common\widgets\Alert;
 
 AppAsset::register($this);
+Yii::$app->language = 'ru';
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 	<head>
 		<meta charset="<?= Yii::$app->charset ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 		<?= Html::csrfMetaTags() ?>
 		<title><?= Html::encode($this->title) ?></title>
 		<?php $this->head() ?>
 	</head>
-	<body ng-app="main" ng-controller="SiteController as site">
+	<body ng-app="main" ng-controller="HeaderController as header">
 		<?php $this->beginBody() ?>
-
+			<div class="content">
 				<?= $this->render("//partial/top") ?>
 					<?= $content ?>
-				<?= $this->render("//partial/bottom") ?>
-
+			</div>
+			<?= $this->render("//partial/bottom") ?>
 		<?php $this->endBody() ?>
 	</body>
 </html>
--- frontend/views/partial/top.php
+++ frontend/views/partial/top.php
@@ -1,53 +1,51 @@
-<div class="slide-menu-element mobile-only" ng-class="{'active':site.slideMenuState}" ng-swipe-left="site.setState(false)">
+<section class="header">
+	<div class="content-wrapper">
+		<header class="header-element">
+			<div class="mobile-only">
+				<div class="slide-dummy-element" ng-swipe-right="header.setState(true)"></div>
+				<div class="row">
+					<div class="col s12">
+						<span class="hamburger-element" ng-click="header.toggleMenu()">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</span>
+						<a class="header-logo" href="/" title="beshitza.ru">
+							<img src="/img/logo_header_mobile.png">
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="desktop-only container container-90">
+				<div class="row">
+					<div class="col l4">
+						<a class="header-logo" href="/" title="beshitza.ru">
+							<img src="/img/logo_header.jpg">
+						</a>
+					</div>
+					<div class="header-menu col l5">
+						<nav class="menu">
+							<a class="item" ng-click="header.scrollTo('section#section1')">отель</a>
+							<a class="item" ng-click="header.scrollTo('section#section2')">номера</a>
+							<a class="item" ng-click="header.scrollTo('section#section3')">ресторан</a>
+							<a class="item" ng-click="header.scrollTo('section#section4')">фитнес-клуб&nbsp</a>
+						</nav>
+					</div>
+					<div class="lang-switcher col l3">
+						<a>
+							in english
+						</a>
+					</div>
+				</div>
+			</div>
+		</header>
+	</div>
+</section>
+<div class="slide-menu-element" ng-class="{'active':header.slideMenuState}" ng-swipe-left="header.setState(false)">
 	<nav class="menu">
-		<a class="item" ng-click="site.setMenuState(false); site.scrollBodyTo('section#section1')">отель</a>
-		<a class="item" ng-click="site.setMenuState(false); site.scrollBodyTo('section#section2')">номера</a>
-		<a class="item" ng-click="site.setMenuState(false); site.scrollBodyTo('section#section3')">ресторан</a>
-		<a class="item" ng-click="site.setMenuState(false); site.scrollBodyTo('section#section4')">фитнес-клуб&nbsp;</a>
+		<a class="item" ng-click="header.setState(false); header.scrollTo('section#section1')">отель</a>
+		<a class="item" ng-click="header.setState(false); header.scrollTo('section#section2')">номера</a>
+		<a class="item" ng-click="header.setState(false); header.scrollTo('section#section3')">ресторан</a>
+		<a class="item" ng-click="header.setState(false); header.scrollTo('section#section4')">фитнес-клуб&nbsp</a>
 	</nav>
 </div>
-<div class="content">
-	<section class="header">
-			<div class="content-wrapper">
-				<header class="header-element">
-					<div class="mobile-only">
-						<div class="slide-dummy-element" ng-swipe-right="site.setState(true)"></div>
-						<div class="row">
-							<div class="col s12">
-								<span class="hamburger-element" ng-click="site.toggleMenu()">
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-								</span>
-								<a class="header-logo" href="/" title="beshitza.ru">
-									<img src="/img/logo_header_mobile.png">
-								</a>
-							</div>
-						</div>
-					</div>
-					<div class="desktop-only container container-90">
-						<div class="row">
-							<div class="col l4">
-								<a class="header-logo" href="/" title="beshitza.ru">
-									<img src="/img/logo_header.jpg">
-								</a>
-							</div>
-							<div class="header-menu col l5">
-								<nav class="menu">
-									<a class="item" ng-click="site.scrollBodyTo('section#section1')">отель</a>
-									<a class="item" ng-click="site.scrollBodyTo('section#section2')">номера</a>
-									<a class="item" ng-click="site.scrollBodyTo('section#section3')">ресторан</a>
-									<a class="item" ng-click="site.scrollBodyTo('section#section4')">фитнес-клуб&nbsp</a>
-								</nav>
-							</div>
-							<div class="lang-switcher col l3">
-								<a>
-									in english
-								</a>
-							</div>
-						</div>
-					</div>
-				</header>
-			</div>
-
-		</section>

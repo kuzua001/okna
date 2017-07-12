0acc3f6eb6868d205b069ffac8063e84e5017925
Ivan Kuznetsov <kuznecov.fd0@yandex.ru>
2017-07-13 00:26:58 +0300

    Исправил ошибку в связи с рефакторнгом контроллера, которую не заметил при прошлом коммите

--- frontend/views/partial/top.php
+++ frontend/views/partial/top.php
@@ -2,10 +2,10 @@
 	<div class="content-wrapper">
 		<header class="header-element">
 			<div class="mobile-only">
-				<div class="slide-dummy-element" ng-swipe-right="header.setState(true)"></div>
+				<div class="slide-dummy-element" ng-swipe-right="site.setMenuState(true)"></div>
 				<div class="row">
 					<div class="col s12">
-						<span class="hamburger-element" ng-click="header.toggleMenu()">
+						<span class="hamburger-element" ng-click="site.toggleMenu()">
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
@@ -25,10 +25,10 @@
 					</div>
 					<div class="header-menu col l5">
 						<nav class="menu">
-							<a class="item" ng-click="header.scrollTo('section#section1')">отель</a>
-							<a class="item" ng-click="header.scrollTo('section#section2')">номера</a>
-							<a class="item" ng-click="header.scrollTo('section#section3')">ресторан</a>
-							<a class="item" ng-click="header.scrollTo('section#section4')">фитнес-клуб&nbsp</a>
+							<a class="item" ng-click="site.scrollBodyTo('section#section1')">отель</a>
+							<a class="item" ng-click="site.scrollBodyTo('section#section2')">номера</a>
+							<a class="item" ng-click="site.scrollBodyTo('section#section3')">ресторан</a>
+							<a class="item" ng-click="site.scrollBodyTo('section#section4')">фитнес-клуб&nbsp</a>
 						</nav>
 					</div>
 					<div class="lang-switcher col l3">
@@ -43,9 +43,9 @@
 </section>
 <div class="slide-menu-element" ng-class="{'active':header.slideMenuState}" ng-swipe-left="header.setState(false)">
 	<nav class="menu">
-		<a class="item" ng-click="header.setState(false); header.scrollTo('section#section1')">отель</a>
-		<a class="item" ng-click="header.setState(false); header.scrollTo('section#section2')">номера</a>
-		<a class="item" ng-click="header.setState(false); header.scrollTo('section#section3')">ресторан</a>
-		<a class="item" ng-click="header.setState(false); header.scrollTo('section#section4')">фитнес-клуб&nbsp</a>
+		<a class="item" ng-click="site.setMenuState(false); site.scrollBodyTo('section#section1')">отель</a>
+		<a class="item" ng-click="site.setMenuState(false); site.scrollBodyTo('section#section2')">номера</a>
+		<a class="item" ng-click="site.setMenuState(false); site.scrollBodyTo('section#section3')">ресторан</a>
+		<a class="item" ng-click="site.setMenuState(false); site.scrollBodyTo('section#section4')">фитнес-клуб&nbsp</a>
 	</nav>
 </div>

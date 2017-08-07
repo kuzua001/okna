5e1e6c9fff6b5bee929d278408840858488c9af5
Ivan Kuznetsov <kuznecov.fd0@yandex.ru>
2017-08-08 00:27:17 +0300

    перепоставил счетчик

--- frontend/views/fitness/index.php
+++ frontend/views/fitness/index.php
@@ -3,6 +3,11 @@ use frontend\components\AppHelper;
 use frontend\widgets\common\MainFitnessSection;
 use frontend\widgets\common\StyleFitnessSection;
 ?>
+<script type="text/javascript">
+	var __cs = __cs || [];
+	__cs.push(["setCsAccount", "cMppfrgKvBqygQv2OcrBmHXGGyui6gOi"]);
+</script>
+<script type="text/javascript" async src="//app.comagic.ru/static/cs.min.js"></script>
 <?php
 
 echo MainFitnessSection::widget([
--- frontend/views/restaurant/index.php
+++ frontend/views/restaurant/index.php
@@ -2,11 +2,6 @@
 use frontend\components\AppHelper;
 use frontend\widgets\common\MainRestaurantSection;
 ?>
-<script type="text/javascript">
-	var __cs = __cs || [];
-	__cs.push(["setCsAccount", "cMppfrgKvBqygQv2OcrBmHXGGyui6gOi"]);
-</script>
-<script type="text/javascript" async src="//app.comagic.ru/static/cs.min.js"></script>
 <?php
 
 echo MainRestaurantSection::widget([
